e742c6e738cad0d5f49688c197a2dfe0cae4c47c
anukiancu <anukiancu@gmail.com>
2021-05-26 22:09:47 +0100

    Added functionality for the random picture message

--- curby/bot.py
+++ curby/bot.py
@@ -1,6 +1,12 @@
 import discord
+import random
+from pathlib import Path
 from discord.ext import commands
+from discord import File
 
+
+pictures = list(Path("Pics").glob("**/*"))
+sanitized_pictures = [str(pic) for pic in pictures]
 bot = commands.Bot(command_prefix=">")
 
 
@@ -16,4 +22,23 @@ async def colour(ctx):
 
 @bot.command()
 async def poyo(ctx):
-    await ctx.send("Poyo! :3")
+
+    todays_picture = random.choice(sanitized_pictures)
+
+    with open(todays_picture, "rb") as fp:
+        discord_file = File(fp, filename="kirby pic.jpg")
+
+    await ctx.send(file=discord_file)
+
+
+@bot.command()
+async def commands(ctx):
+    command_help = """
+    ```
+Hi. Am Curby. Here's a list of commands:
+>hello: Greets you.
+>colour: I say my colour.
+>poyo: Sends a random picture of me.
+```
+"""
+    await ctx.send(command_help)
